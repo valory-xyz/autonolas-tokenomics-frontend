967fe230e0e917ca505bfaf5ac8149f7fd74b002
mohandast52 <mohandast52@gmail.com>
2023-03-19 16:38:21 +0530

    update form

--- common-util/DynamicFieldsForm/index.jsx
+++ common-util/DynamicFieldsForm/index.jsx
@@ -1,112 +1,125 @@
+import React from 'react';
 import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons';
-import { Button, Form, Input } from 'antd/lib';
+import {
+  Button, Form, Input, InputNumber, Select, Space,
+} from 'antd/lib';
+import styled from 'styled-components';
 
-const formItemLayout = {
-  labelCol: { xs: { span: 24 }, sm: { span: 4 } },
-  wrapperCol: { xs: { span: 24 }, sm: { span: 20 } },
-};
-const formItemLayoutWithOutLabel = {
-  wrapperCol: {
-    xs: { span: 24, offset: 0 },
-    sm: { span: 20, offset: 4 },
-  },
-};
+export const DynamicFormContainer = styled.div`
+  max-width: 700px;
+`;
 
 export const DynamicFieldsForm = () => {
+  const [form] = Form.useForm();
   const onFinish = (values) => {
     console.log('Received values of form:', values);
   };
+  const handleChange = () => {
+    if (form.getFieldValue('units').length === 0) {
+      return;
+    }
+
+    form.setFieldsValue({
+      units: [],
+    });
+  };
 
   return (
-    <Form
-      name="dynamic_form_item"
-      {...formItemLayoutWithOutLabel}
-      onFinish={onFinish}
-    >
-      <Form.List
-        name="names"
-        rules={[
-          {
-            validator: async (_, names) => {
-              if (!names || names.length < 2) {
-                return Promise.reject(new Error('At least 2 passengers'));
-              }
-              return Promise.resolve();
-            },
-          },
-        ]}
+    <DynamicFormContainer>
+      <Form
+        form={form}
+        name="dynamic_form_complex"
+        onFinish={onFinish}
+        autoComplete="off"
       >
-        {(fields, { add, remove }, { errors }) => (
-          <>
-            {fields.map((field, index) => (
-              <Form.Item
-                {...(index === 0 ? formItemLayout : formItemLayoutWithOutLabel)}
-                label={index === 0 ? 'Passengers' : ''}
-                required={false}
-                key={field.key}
-              >
-                <Form.Item
-                  {...field}
-                  validateTrigger={['onChange', 'onBlur']}
-                  rules={[
-                    {
-                      required: true,
-                      whitespace: true,
-                      message:
-                        "Please input passenger's name or delete this field.",
-                    },
-                  ]}
-                  noStyle
-                >
-                  <Input
-                    placeholder="passenger name"
-                    style={{
-                      width: '60%',
-                    }}
-                  />
-                </Form.Item>
-                {fields.length > 1 ? (
-                  <MinusCircleOutlined
-                    className="dynamic-delete-button"
-                    onClick={() => remove(field.name)}
-                  />
-                ) : null}
-              </Form.Item>
-            ))}
-            <Form.Item>
-              <Button
-                type="dashed"
-                onClick={() => add()}
-                style={{
-                  width: '60%',
-                }}
-                icon={<PlusOutlined />}
-              >
-                Add field
-              </Button>
-              <Button
-                type="dashed"
-                onClick={() => {
-                  add('The head item', 0);
-                }}
-                style={{
-                  width: '60%',
-                  marginTop: '20px',
-                }}
-                icon={<PlusOutlined />}
-              >
-                Add field at head
-              </Button>
+        <Form.Item
+          name="area"
+          label="Area"
+          rules={[{ required: true, message: 'Missing area' }]}
+        >
+          <Select
+            options={[
+              { label: 'Beijing', value: 'Beijing' },
+              { label: 'Shanghai', value: 'Shanghai' },
+            ]}
+            onChange={handleChange}
+          />
+        </Form.Item>
+
+        <Form.List
+          name="units"
+          rules={[
+            {
+              validator: async (_, units) => {
+                if (!units || units?.length === 0) {
+                  return Promise.reject(
+                    new Error('At least 1 unit is required'),
+                  );
+                }
+                return Promise.resolve();
+              },
+            },
+          ]}
+        >
+          {(fields, { add, remove }, { errors }) => (
+            <>
+              {fields.map((field) => (
+                <Space key={field.key} align="baseline">
+                  <Form.Item
+                    noStyle
+                    shouldUpdate={(prevValues, curValues) => prevValues.area !== curValues.area
+                      || prevValues.units !== curValues.units}
+                  >
+                    {() => (
+                      <Form.Item
+                        {...field}
+                        label="Service ID"
+                        name={[field.name, 'serviceId']}
+                        rules={[
+                          { required: true, message: 'Please add service ID' },
+                        ]}
+                      >
+                        <InputNumber style={{ width: 200, marginRight: 32 }} />
+                      </Form.Item>
+                    )}
+                  </Form.Item>
+
+                  <Form.Item
+                    {...field}
+                    label="Amount"
+                    name={[field.name, 'amount']}
+                    rules={[{ required: true, message: 'Please add amount' }]}
+                  >
+                    <Input />
+                  </Form.Item>
+
+                  <MinusCircleOutlined onClick={() => remove(field.name)} />
+                </Space>
+              ))}
+
+              {/* show error above the "Add Unit" button */}
               <Form.ErrorList errors={errors} />
-            </Form.Item>
-          </>
-        )}
-      </Form.List>
-      <Form.Item>
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
+
+              <Form.Item>
+                <Button
+                  type="dashed"
+                  onClick={() => add()}
+                  block
+                  icon={<PlusOutlined />}
+                >
+                  Add Unit
+                </Button>
+              </Form.Item>
+            </>
+          )}
+        </Form.List>
+
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </DynamicFormContainer>
   );
 };
