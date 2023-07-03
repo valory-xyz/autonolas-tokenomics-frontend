9bf10dc3828a99485616c098c871dd2008eac2e6
mohandast52 <mohandast52@gmail.com>
2023-07-03 17:39:04 +0530

    tiny

--- components/Layout/Footer.jsx
+++ components/Layout/Footer.jsx
@@ -2,8 +2,11 @@ import Link from 'next/link';
 import { Footer as CommonFooter } from '@autonolas/frontend-library';
 import Socials from './Socials';
 
+const ContractInfo = () => 'Abcd';
+
 const Footer = () => (
   <CommonFooter
+    leftContent={<ContractInfo />}
     rightContent={<Socials />}
     centerContent={(
       <>
