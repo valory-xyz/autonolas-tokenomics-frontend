8fa9960a55645da5aeca485fc770e88b3dadf244
mohandast52 <mohandast52@gmail.com>
2023-10-19 17:45:57 +0530

    add ofac sanctioned countries and addresses

--- middleware.js
+++ middleware.js
@@ -1,16 +1,14 @@
 import { NextResponse } from 'next/server';
 
-const invalidCounties = ['US'];
+const invalidCounties = ['IN'];
 
 /**
  * Middleware to validate the country and address based on OFAC and other legal requirements
  *
  * @param {NextRequest} req
  */
-export default function validateCountyAndAddressMiddleware(req) {
+export default function validateCountyMiddleware(req) {
   const country = req.geo?.country;
-
-  console.log({ req, country: req.geo?.country });
 
   if (invalidCounties.includes(country)) {
     return NextResponse.error('Blocked for legal reasons', { status: 451 });
