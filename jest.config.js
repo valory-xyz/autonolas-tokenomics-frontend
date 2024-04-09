a4017e0b5fe7b2239424db557c58d5af498b130c
Josh Miller <mlxndrj@gmail.com>
2024-04-09 15:14:43 +0100

    Update linting command and test dependencies

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,28 @@
-module.exports = {
-  verbose: true,
-  setupFilesAfterEnv: ['./jest.setup.js'],
-  moduleNameMapper: {
-    '\\.(css|less|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
-    '\\.(gif|ttf|eot|svg)$': '<rootDir>/__mocks__/fileMock.js',
-  },
+const nextJest = require('next/jest');
+
+/** @type {import('jest').Config} */
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './',
+});
+
+// Add any custom config to be passed to Jest
+const config = {
+  coverageProvider: 'v8',
   testEnvironment: 'jsdom',
+  // Add more setup options before each test is run
+  // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 };
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(config);
+
+// module.exports = {
+//   verbose: true,
+//   setupFilesAfterEnv: ['./jest.setup.js'],
+//   moduleNameMapper: {
+//     '\\.(css|less|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
+//     '\\.(gif|ttf|eot|svg)$': '<rootDir>/__mocks__/fileMock.js',
+//   },
+//   testEnvironment: 'jsdom',
+// };
