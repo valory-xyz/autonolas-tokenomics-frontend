10f7f7446dcb98d62a4edd135d61a052d650b0b5
Josh Miller <mlxndrj@gmail.com>
2024-04-09 16:07:20 +0100

    Refactor Jest configuration

--- jest.config.js
+++ jest.config.js
@@ -2,27 +2,13 @@ const nextJest = require('next/jest');
 
 /** @type {import('jest').Config} */
 const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  // Don't change, this is needed to load next.config.js and .env config
   dir: './',
 });
 
-// Add any custom config to be passed to Jest
 const config = {
   coverageProvider: 'v8',
   testEnvironment: 'jsdom',
-  // Add more setup options before each test is run
-  // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 };
 
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 module.exports = createJestConfig(config);
-
-// module.exports = {
-//   verbose: true,
-//   setupFilesAfterEnv: ['./jest.setup.js'],
-//   moduleNameMapper: {
-//     '\\.(css|less|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
-//     '\\.(gif|ttf|eot|svg)$': '<rootDir>/__mocks__/fileMock.js',
-//   },
-//   testEnvironment: 'jsdom',
-// };
