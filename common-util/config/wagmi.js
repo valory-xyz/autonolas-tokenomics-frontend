8700e1466339dc6eec9bbb59f1803238b086dd21
Josh Miller <mlxndrj@gmail.com>
2024-04-10 09:26:10 +0100

    Update walletConnect metadata in wagmiConfig

--- common-util/config/wagmi.js
+++ common-util/config/wagmi.js
@@ -7,7 +7,7 @@ export const projectId = process.env.NEXT_PUBLIC_WALLET_PROJECT_ID;
 export const SUPPORTED_CHAINS = [mainnet, goerli];
 export const SUPPORTED_CHAIN_IDS = SUPPORTED_CHAINS.map((chain) => chain.id);
 
-const metadata = {
+const walletConnectMetadata = {
   name: 'OLAS Tokenomics',
   description: 'OLAS Tokenomics Web3 Modal',
   url: 'https://tokenomics.olas.network', // origin must match your domain & subdomain
@@ -19,7 +19,11 @@ export const wagmiConfig = createConfig({
   chains: SUPPORTED_CHAINS,
   connectors: [
     injected(),
-    walletConnect({ projectId, metadata, showQrModal: false }),
+    walletConnect({
+      projectId,
+      metadata: walletConnectMetadata,
+      showQrModal: false,
+    }),
     safe(),
   ],
   transports: SUPPORTED_CHAINS.reduce(
